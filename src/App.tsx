134d4f6495f14117503c346be5a24739dc4bc6aa
jvillegasl <jvillegasl@uni.pe>
2023-04-02 16:27:11 -0500

    update: misc

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.scss";
+import { TicTacToe } from "./components";
 
 function App() {
-    return <div className="c-app">
-    </div>;
+    return (
+        <div className="c-app">
+            <h1 className="c-app__title">Tic-Tac-Toe</h1>
+
+            <div className="c-app__body">
+                <TicTacToe />
+            </div>
+        </div>
+    );
 }
 
 export default App;
